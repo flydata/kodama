b285eadc538c1b7046fdf6746d7fefad0256491b
Masashi Miyazaki <mmasashicom@gmail.com>
2015-08-04 19:01:59 +0000

    @T3408:R Support set cipher api

--- example/test_client.rb
+++ example/test_client.rb
@@ -18,6 +18,7 @@ OptionParser.new do |opt|
   opt.on('-h VALUE', 'host')         {|v| option[:host] = v}
   opt.on('--port VALUE', 'port')     {|v| option[:port] = v.to_i}
   opt.on('--ssl-ca VALUE', 'ssl_ca') {|v| option[:ssl_ca] = v}
+  opt.on('--ssl-cipher VALUE', 'ssl_cipher') {|v| option[:ssl_cipher] = v}
   # --help for showing help
   opt.parse!(ARGV)
 end
@@ -48,34 +49,42 @@ class Worker
   end
 end
 
-worker = Worker.new
 
 SLEEP_TIME = 3
-puts "===== Kodama client start ====="
-Kodama::Client.start(conn_info) do |c|
-  puts conn_info
-  puts "== Start connecting to #{conn_info.merge(password: (conn_info[:password].nil? ? '<not set>' : 'xxxx' ))}"
-  c.binlog_position_file = 'position.log'
-  c.sent_binlog_position_file = 'sent_position.log'
-  if option[:ssl_ca]
-    c.ssl_ca = option[:ssl_ca]
-  end
+def start_kodama(conn_info, option)
+  worker = Worker.new
+  puts "===== Kodama client start ====="
+  Kodama::Client.start(conn_info) do |c|
+    puts conn_info
+    puts "== Start connecting to #{conn_info.merge(password: (conn_info[:password].nil? ? '<not set>' : 'xxxx' ))}"
+    c.binlog_position_file = 'position.log'
+    c.sent_binlog_position_file = 'sent_position.log'
+    if option[:ssl_ca]
+      c.ssl_ca = option[:ssl_ca]
+      c.ssl_cipher = option[:ssl_cipher]
+    end
 
-  c.on_row_event do |event|
-    worker.perform(event)
-    sleep SLEEP_TIME
+    c.on_row_event do |event|
+      worker.perform(event)
+      sleep SLEEP_TIME
+    end
+    c.on_query_event do |event|
+      puts "query: #{event.query}"
+      sleep SLEEP_TIME
+    end
+    c.on_rotate_event do |event|
+      puts "rotate:#{event.binlog_file} #{event.binlog_pos}"
+      sleep SLEEP_TIME
+    end
+    c.on_table_map_event do |event|
+      puts "table_map: #{event.table_name}"
+      sleep SLEEP_TIME
+    end
   end
-  c.on_query_event do |event|
-    puts "query: #{event.query}"
-    sleep SLEEP_TIME
-  end
-  c.on_rotate_event do |event|
-    puts "rotate:#{event.binlog_file} #{event.binlog_pos}"
-    sleep SLEEP_TIME
-  end
-  c.on_table_map_event do |event|
-    puts "table_map: #{event.table_name}"
-    sleep SLEEP_TIME
-  end
- 
+end
+
+begin
+  start_kodama(conn_info, option)
+rescue => e
+  puts "Error!:#{e}"
 end
